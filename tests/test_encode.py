32a2493e36d8637a63740076acb111ccb1db7df6
Atte Lautanala <atte.lautanala@gmail.com>
2017-07-16 00:32:07 +0300

    Use pytest style in tests

--- tests/test_encode.py
+++ tests/test_encode.py
@@ -1,44 +1,43 @@
-#!/usr/bin/env python
 from __future__ import absolute_import
-from unittest import main, TestCase
+
+import pytest
 
 from blurhash import encode
 
 
-class BlurhashEncodeTestCase(TestCase):
+def test_encode_file():
+    with open('pic2.png', 'rb') as image_file:
+        result = encode(image_file, 4, 3)
 
-    def test_encode_file(self):
-        with open('pic2.png', 'rb') as image_file:
-            result = encode(image_file, 4, 3)
-
-        self.assertEqual(result, b'jaMpGa00;;8hjal6hr8yhlmYhkqG')
-
-    def test_encode_with_filename(self):
-        result = encode('pic2.png', 4, 3)
-        self.assertEqual(result, b'jaMpGa00;;8hjal6hr8yhlmYhkqG')
-
-    def test_encode_black_and_white_picture(self):
-        result = encode('pic2_bw.png', 4, 3)
-        self.assertEqual(result, b'j9Ea2w00;;4hPch4ll8yh4KXh4Cp')
-
-    def test_invalid_image(self):
-        with self.assertRaises(IOError):
-            encode('encode.c', 4, 3)
-
-    def test_invalid_x_components(self):
-        with self.assertRaises(ValueError):
-            encode('pic2.png', 9, 3)
-
-        with self.assertRaises(ValueError):
-            encode('pic2.png', 0, 3)
-
-    def test_invalid_y_components(self):
-        with self.assertRaises(ValueError):
-            encode('pic2.png', 4, 9)
-
-        with self.assertRaises(ValueError):
-            encode('pic2.png', 4, 0)
+    assert result == b'jaMpGa00;;8hjal6hr8yhlmYhkqG'
 
 
-if __name__ == '__main__':
-    main()
+def test_encode_with_filename():
+    result = encode('pic2.png', 4, 3)
+    assert result == b'jaMpGa00;;8hjal6hr8yhlmYhkqG'
+
+
+def test_encode_black_and_white_picture():
+    result = encode('pic2_bw.png', 4, 3)
+    assert result == b'j9Ea2w00;;4hPch4ll8yh4KXh4Cp'
+
+
+def test_invalid_image():
+    with pytest.raises(IOError):
+        encode('encode.c', 4, 3)
+
+
+def test_invalid_x_components():
+    with pytest.raises(ValueError):
+        encode('pic2.png', 9, 3)
+
+    with pytest.raises(ValueError):
+        encode('pic2.png', 0, 3)
+
+
+def test_invalid_y_components():
+    with pytest.raises(ValueError):
+        encode('pic2.png', 4, 9)
+
+    with pytest.raises(ValueError):
+        encode('pic2.png', 4, 0)
