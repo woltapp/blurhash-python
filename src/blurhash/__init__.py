a90c1c094f2a4a57a9f262a62f1fba8c9f5dd6fb
Atte Lautanala <atte.lautanala@gmail.com>
2018-01-30 01:42:22 +0200

    Evaluate chain from zip lazily

--- src/blurhash/__init__.py
+++ src/blurhash/__init__.py
@@ -14,7 +14,7 @@ def encode(image_file, x_components, y_components):
     red_band = image.getdata(band=0)
     green_band = image.getdata(band=1)
     blue_band = image.getdata(band=2)
-    rgb_data = list(chain(*zip(red_band, green_band, blue_band)))
+    rgb_data = list(chain.from_iterable(zip(red_band, green_band, blue_band)))
     width, height = image.size
     image.close()
 
