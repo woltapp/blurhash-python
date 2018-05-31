003c1c5de19da47aafea6199bbd058c46c621d44
Atte Lautanala <atte.lautanala@gmail.com>
2018-05-31 14:52:41 +0300

    Add documentation to setup

--- setup.py
+++ setup.py
@@ -4,9 +4,19 @@ from __future__ import absolute_import
 from setuptools import setup
 
 
+with open('README.md', 'r') as readme_file:
+    long_description = readme_file.read()
+
+
 setup(
     name='blurhash-python',
+    description='BlurHash encoder implementation for Python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/woltapp/blurhash-python',
     version='1.0.0',
+    author='Atte Lautanala',
+    author_email='atte.lautanala@wolt.com',
     packages=['blurhash'],
     package_dir={'': 'src'},
     install_requires=[
@@ -19,5 +29,15 @@ setup(
         'cffi>=1.9.1,<1.12.0'
     ],
     cffi_modules=['src/build_blurhash.py:ffibuilder'],
-    tests_require=['pytest==3.3.2']
+    tests_require=['pytest==3.3.2'],
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Progarmming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    )
 )
