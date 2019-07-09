96117f2b370274a792e1b06dcd07ff285821a005
Atte Lautanala <atte.lautanala@gmail.com>
2019-07-09 14:04:11 +0300

    Add scripts for building Python packages

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -euo pipefail
+
+TMPDIST="$(mktemp -d)"
+trap "rm -rf '$TMPDIST'" EXIT
+
+for pybin in /opt/python/cp{27,35,36,37}-cp*/bin; do
+    "${pybin}/pip" wheel -w "$TMPDIST" ".[testing]"
+done
+
+for whl in "$TMPDIST"/blurhash_python*.whl; do
+    auditwheel repair "$whl" --plat "$PLAT" -w dist
+    rm "$whl"
+done
+
+for pybin in /opt/python/cp{27,35,36,37}-cp*/bin; do
+    "${pybin}/pip" install --no-index -f dist -f "$TMPDIST" "blurhash-python[testing]"
+    "${pybin}/pytest"
+done
---
+++ docker-build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euo pipefail
+
+function docker-run {
+    local workdir="/tmp/blurhash-python"
+    docker run --rm -e "PLAT=$1" -v "$(pwd)":"$workdir" -w $workdir \
+        "quay.io/pypa/$1" "${@:2}"
+}
+
+docker-run manylinux1_x86_64 ./build.sh
+docker-run manylinux2010_x86_64 ./build.sh
+docker-run manylinux1_i686 linux32 ./build.sh
