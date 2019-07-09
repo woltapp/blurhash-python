5195357532e07d6f2545c0b4deedbfda458ed3e8
Atte Lautanala <atte.lautanala@gmail.com>
2019-07-09 14:25:52 +0300

    Fix typo in classifiers

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Progarmming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
