472fb5f40e61cae9b9db41eeb0c9eaee1959e96a
Atte Lautanala <atte.lautanala@gmail.com>
2019-12-02 12:27:01 +0200

    Enable Python 3.8 build

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 TMPDIST="$(mktemp -d)"
 trap "rm -rf '$TMPDIST'" EXIT
 
-for pybin in /opt/python/cp{27,35,36,37}-cp*/bin; do
+for pybin in /opt/python/cp{27,35,36,37,38}-cp*/bin; do
     "${pybin}/pip" wheel -w "$TMPDIST" ".[testing]"
 done
 
@@ -13,7 +13,7 @@ for whl in "$TMPDIST"/blurhash_python*.whl; do
     rm "$whl"
 done
 
-for pybin in /opt/python/cp{27,35,36,37}-cp*/bin; do
+for pybin in /opt/python/cp{27,35,36,37,38}-cp*/bin; do
     "${pybin}/pip" install --no-index -f dist -f "$TMPDIST" "blurhash-python[testing]"
     "${pybin}/pytest"
 done
