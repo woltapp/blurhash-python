c211035430eac4c81ffbb46d7b7451167bcc407c
Narasimha1997 <yogaimpression@gmail.com>
2020-11-12 12:28:36 +0530

    Added RGB support

--- src/blurhash/__init__.py
+++ src/blurhash/__init__.py
@@ -1,14 +1,21 @@
 from __future__ import absolute_import
 from itertools import chain
 
+
 from PIL import Image
+
 from six.moves import zip
 
-from ._encode import ffi as _ffi, lib as _lib
+from ._functions import ffi as _ffi, lib as _lib
 from ._version import version as __version__
 
 
-__all__ = 'encode', '__version__',
+__all__ = 'encode', 'decode', '__version__',
+
+_pixel_modes = {
+    "RGB" : 3,
+    "RGBA" : 4
+}
 
 
 def encode(image_file, x_components, y_components):
@@ -34,3 +41,40 @@ def encode(image_file, x_components, y_components):
         raise ValueError('Invalid x_components or y_components')
 
     return _ffi.string(result).decode()
+
+def decode(blurhash, width, height, punch, mode = 'RGB'):
+
+    if not mode in _pixel_modes:
+        raise ValueError("Invalid value for argument mode, must be either 'RGB' or 'RGBA'")
+
+    channels = _pixel_modes[mode]
+
+    blurhash_str = _ffi.new('char[]', bytes(blurhash, "utf-8"))
+
+    if not _lib.is_valid_blurhash(blurhash_str) :
+        raise ValueError("{} is not a valid blurhash".format(blurhash))
+
+    width_int = _ffi.cast('int', width)
+    height_int = _ffi.cast('int', height)
+    punch_int = _ffi.cast('int', punch)
+    channels_int = _ffi.cast('int', channels)
+
+    pixels = _lib.create_pixels_from_blurhash(blurhash_str, width_int, height_int,
+                                    punch_int, channels)
+    
+    if pixels == _ffi.NULL :
+        raise Exception("Failed to decode blurhash {}".format(blurhash))
+
+    #garbage collection of pixel buffer
+    pixels = _ffi.gc(pixels, _lib.free_pixel_array)
+
+    pixels_buffer = _ffi.buffer(pixels, width * height * channels)
+
+    image = Image.frombuffer(mode, (width, height), pixels_buffer)
+
+    return image
+
+def is_valid_blurhash(blurhash) :
+
+    blurhash_str = _ffi.new('char[]', bytes(blurhash, 'utf-8'))
+    return bool(_lib.is_valid_blurhash(blurhash_str))
--- src/build_blurhash.py
+++ src/build_blurhash.py
@@ -5,10 +5,13 @@ import sys
 
 
 ffibuilder = cffi.FFI()
-ffibuilder.set_source('blurhash._encode', '''
-    const char* blurHashForPixels(int xComponents, int yComponents, int width,
-                                  int height, uint8_t *rgb,
-                                  size_t bytesPerRow);
+ffibuilder.set_source('blurhash._functions', '''
+
+    const char* blurHashForPixels(int x_components, int y_components, int width, int height,
+                                uint8_t * rgb, size_t bytesPerRow);
+    uint8_t * decode(const char* blurhash, int width, int height, int punch, int n_channels);
+    void freePixelArray(uint8_t * pixelsPtr);
+    
 
     const char* create_hash_from_pixels(int x_components, int y_components,
                                        int width, int height, uint8_t* rgb,
@@ -16,12 +19,33 @@ ffibuilder.set_source('blurhash._encode', '''
         return blurHashForPixels(x_components, y_components, width, height,
                                  rgb, bytes_per_row);
     }
-''', sources=['src/encode.c'], extra_compile_args=['-std=gnu99'] if sys.platform != 'win32' else [])
+
+    uint8_t * create_pixels_from_blurhash(const char * blurhash, int width, int height, 
+                                     int punch, int n_channels){
+            return decode(blurhash, width, height, punch, n_channels);
+    }
+
+    int is_valid_blurhash(const char * blurhash) {
+        return isValidBlurhash(blurhash);
+    }
+
+    void free_pixel_array(uint8_t * pixel_ptr) {
+        freePixelArray(pixel_ptr);
+    }
+''', sources=['src/encode.c', 'src/decode.c'], extra_compile_args=['-std=gnu99'] if sys.platform != 'win32' else [])
+
 ffibuilder.cdef('''
     const char* create_hash_from_pixels(int x_components, int y_components,
                                        int width, int height, uint8_t* rgb,
                                        size_t bytes_per_row);
+    uint8_t * create_pixels_from_blurhash(const char * blurhash, int width, int height,
+                                    int punch, int nChannels);
+
+    int is_valid_blurhash(const char * blurhash);
+    
+    void free_pixel_array(uint8_t * pixel_ptr);
 ''')
+
 
 if __name__ == '__main__':
     ffibuilder.compile()
