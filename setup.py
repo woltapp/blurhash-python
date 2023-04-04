7df0df71b5f901e7864197e7ed9fe69a9bb1c6b5
Atte Lautanala <atte.lautanala@wolt.com>
2023-04-04 10:28:06 +0300

    Drop support for Python 3.6, which has reached EOL

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python
-from __future__ import absolute_import
-
 from setuptools import setup
 
 
@@ -44,7 +42,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
