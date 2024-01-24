9a83b334f286d9682f8fb2ea4b00989ed2924e0e
Atte Lautanala <atte.lautanala@wolt.com>
2024-01-24 13:39:36 +0200

    Build wheels for aarch64

--- docker-build.sh
+++ docker-build.sh
@@ -2,10 +2,14 @@
 set -euo pipefail
 
 function docker-run {
+    local platform="$1"
+    local arch="$2"
     local workdir="/tmp/blurhash-python"
-    docker run --rm -e "PLAT=$1" -v "$(pwd)":"$workdir" -w $workdir \
-        -u "$(id -u):$(id -g)" "quay.io/pypa/$1" "${@:2}"
+
+    docker run --rm -e "PLAT=$platform" -v "$(pwd)":"$workdir" -w $workdir \
+        -u "$(id -u):$(id -g)" --arch "$arch" "quay.io/pypa/$platform" "${@:3}"
 }
 
 python -m build -s
-docker-run manylinux_2_28_x86_64 ./build.sh
+docker-run manylinux_2_28_x86_64 amd64 ./build.sh
+docker-run manylinux_2_28_aarch64 arm64 ./build.sh
